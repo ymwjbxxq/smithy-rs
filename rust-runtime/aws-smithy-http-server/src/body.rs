0fb2254c78c8397f857e14f9cee07d47900e9273
david-perez <d@vidp.dev>
2022-01-28 15:59:28 +0100

    Attempt to not expose `axum_core::BoxBody` in our public API
    However, it would require us to not use `IntoResponse` from axum.

--- rust-runtime/aws-smithy-http-server/src/body.rs
+++ rust-runtime/aws-smithy-http-server/src/body.rs
@@ -3,6 +3,35 @@
  * SPDX-License-Identifier: Apache-2.0.
  */
 
+// Parts of this code were copied and then modified from Tokio's Axum.
+
+/* Copyright (c) 2021 Tower Contributors
+ *
+ * Permission is hereby granted, free of charge, to any
+ * person obtaining a copy of this software and associated
+ * documentation files (the "Software"), to deal in the
+ * Software without restriction, including without
+ * limitation the rights to use, copy, modify, merge,
+ * publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software
+ * is furnished to do so, subject to the following
+ * conditions:
+ *
+ * The above copyright notice and this permission notice
+ * shall be included in all copies or substantial portions
+ * of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF
+ * ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED
+ * TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+ * PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT
+ * SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+ * IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ */
+
 //! HTTP body utilities.
 #[doc(no_inline)]
 pub use http_body::Body as HttpBody;
@@ -10,8 +39,34 @@ pub use http_body::Body as HttpBody;
 #[doc(no_inline)]
 pub use hyper::body::Body;
 
-#[doc(no_inline)]
-pub use axum_core::body::{boxed, BoxBody};
+/// A boxed [`Body`] trait object.
+///
+/// This is used in as the response body type for applications. It's necessary to unify multiple
+/// response bodies types into one.
+pub type BoxBody = http_body::combinators::UnsyncBoxBody<bytes::Bytes, crate::Error>;
+
+/// Convert a [`http_body::Body`] into a [`BoxBody`].
+#[doc(hidden)]
+pub fn boxed<B>(body: B) -> BoxBody
+where
+    B: http_body::Body<Data = bytes::Bytes> + Send + 'static,
+    B::Error: Into<crate::BoxError>,
+{
+    try_downcast(body).unwrap_or_else(|body| body.map_err(crate::Error::new).boxed_unsync())
+}
+
+fn try_downcast<T, K>(k: K) -> Result<T, K>
+where
+    T: 'static,
+    K: Send + 'static,
+{
+    let mut k = Some(k);
+    if let Some(k) = <dyn std::any::Any>::downcast_mut::<Option<T>>(&mut k) {
+        Ok(k.take().unwrap())
+    } else {
+        Err(k.unwrap())
+    }
+}
 
 pub(crate) fn empty() -> BoxBody {
     boxed(http_body::Empty::new())
@@ -19,6 +74,7 @@ pub(crate) fn empty() -> BoxBody {
 
 /// Convert a generic [`Body`] into a [`BoxBody`]. This is used by the codegen to
 /// simplify the generation logic.
+#[doc(hidden)]
 pub fn to_boxed<B>(body: B) -> BoxBody
 where
     Body: From<B>,
